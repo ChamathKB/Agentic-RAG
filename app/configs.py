746531505053560dbf02bc18ca8528c475637ee5
ChamathKB <chamathkbatta@gmail.com>
2024-11-21 21:10:45 +0530

    refactor: update config file

--- app/configs.py
+++ app/configs.py
@@ -1,16 +1,18 @@
 import os
 
-OPENAI_MODEL=os.getenv("OPENAI_MODEL", "gpt-4o-mini")
-EMBEDDING_MODEL=os.getenv("EMBEDDING_MODEL", "text-embedding-3-large")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
+EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-3-large")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-QDRANT_URL=os.getenv("QDRANT_URL", "http://localhost:6333")
+QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
 
-MONGO_URL= os.getenv("MONGO_URL", "mongodb://localhost:27017")
-MONGO_DB_NAME= os.getenv("MONGO_DB_NAME", "agentic_rag_db")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "agentic_rag_db")
 
-REDIS_URL= os.getenv("REDIS_URL", "redis://localhost:6379")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
-MLFLOW_TRACKING_URI= os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
 
-UPLOAD_DIR="uploads"
+UPLOAD_DIR = "uploads"
+
+SERPER_API_KEY = os.getenv("SERPER_API_KEY")
