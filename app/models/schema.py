ca5dd9825756ad54f52ef882b8700060dba2c0c9
ChamathKB <chamathkbatta@gmail.com>
2025-01-31 00:51:56 +0530

    refactor(tools): update docstrings and type hints

--- app/models/schema.py
+++ app/models/schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from langchain_core.documents import Document
 
 class Query(BaseModel):
@@ -12,3 +12,7 @@ class DocIds(BaseModel):
 
 class Docs(BaseModel):
     docs: list[Document]
+
+class GetCurrentWeatherCheckInput(BaseModel):
+    # Check the input for Weather
+    location: str = Field(..., description = "The name of the location name for which we need to find the weather")
